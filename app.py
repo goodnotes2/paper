616071c567ead9f65c4d6205515357a48769a08f
goodnotes <jjong8649@gmail.com>
2026-02-11 10:32:16 +0900

    Fix Flask typo and enhance calculator UI22

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask import Flask, render_template, request, session, redirect, url_for
 import os
 from datetime import datetime
 
 app = Flask(__name__)
-app.secret_key = 'paper_system_secure_v5_fixed'
+app.secret_key = 'paper_system_final_v6'
 
 SITE_PASSWORD = "03877"
 cached_data = []
